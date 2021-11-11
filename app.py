d79e07a56e984af631c2699cd6139e27265201cd
henriquelobato <iklobato1@gmail.com>
2021-11-11 16:35:00 -0300

    add: fixed vessels

--- app.py
+++ app.py
@@ -2,39 +2,59 @@ from flask import Flask, jsonify
 from faker import Faker
 from flask_cors import CORS
 import random
-from global_land_mask import globe
 from uuid import uuid4
 fake = Faker()
 
 app = Flask(__name__)
 CORS(app)
 
+positions = [
+    (31.322174, 32.239129),
+    (31.324851, 32.343782),
+    (36.018881, -5.440807),
+    (35.962376, -6.002181),
+    (35.926254, -5.465578),
+    (-22.990278, -43.173343),
+    (-22.988333, -43.171667),
+    (-22.752565, -43.142199),
+    (1.137489, 103.772821),
+    (1.472769, 103.796110),
+    (1.476490, 103.805328),
+    (8.614388, 111.579314),
+    (30.209297, 120.725029),
+    (30.208889, 120.724444),
+    (30.557001, 121.531522),
+    (31.049639, 122.447354),
+    (31.448518, 122.896698),
+    (42.017706, -138.981602),
+    (33.999203, -128.610508),
+    (37.659521, -122.279078),
+    (30.316922, -119.149503),
+    (48.167490, -60.568659),
+    (48.166667, -60.566667),
+    (41.725496, -67.506965),
+    (12.223368, -45.131178),
+]
+print('Positions:', len(positions))
+
 vessels = []
 
 
 def generate_vessels():
-    while True:
-        lat, lon = fake.latlng()
-        if globe.is_ocean(float(lat), float(lon)):
-            vessels.append({
-                'id': str(uuid4()),
-                'latitude': float(lat),
-                'longitude': float(lon),
-                'type': random.choice(['cargo ship', 'tourism', 'fishing', 'tanker', 'tug']),
-                'name': f'{fake.company()}',
-                'size': random.randint(20, 250),
-                'country': fake.country(),
-                'from': fake.country(),
-                'to': fake.country(),
-                'complete': f'{random.randint(20, 85)}%',
-                'speed': f'{random.randint(20, 90)} MPH'
-            })
-        if len(vessels) == 10:
-            print('Vessels created!')
-            return True
-
-
-generate_vessels()
+    for lat, lon in positions:
+        vessels.append({
+            'id': str(uuid4()),
+            'latitude': float(lat),
+            'longitude': float(lon),
+            'type': random.choice(['cargo ship', 'tourism', 'fishing', 'tanker', 'tug']),
+            'name': f'{fake.company()}',
+            'size': random.randint(20, 250),
+            'country': fake.country(),
+            'from': fake.country(),
+            'to': fake.country(),
+            'complete': f'{random.randint(20, 85)}%',
+            'speed': f'{random.randint(20, 90)} MPH'
+        })
 
 
 def move_vessels():
@@ -43,12 +63,12 @@ def move_vessels():
         vessel['latitude'] += movement
         vessel['longitude'] += movement
         try:
-            current_porcentage = int(vessel['complete'][:2])
-            add_porcentage = random.random()
-            if current_porcentage + add_porcentage >= 100:
+            current_percentage = int(vessel['complete'][:2])
+            add_percentage = random.random()
+            if current_percentage + add_percentage >= 100:
                 vessel['complete'] = 'COMPLETE'
             else:
-                vessel['complete'] = f'{round(current_porcentage + add_porcentage, 2)}%'
+                vessel['complete'] = f'{round(current_percentage + add_percentage, 2)}%'
         except ValueError:
             continue
     print('Moved!')
